02169dbd9b18fbcde9b3811cd0da9d06ec04bc68
aerox0 <maerox1@gmail.com>
2022-09-19 14:41:38 +0300

    chore: rename user -> userModel

--- apps/api/src/modules/user/mod.rs
+++ apps/api/src/modules/user/mod.rs
@@ -1,1 +1,32 @@
-pub mod model;
+use async_graphql::{InputObject, SimpleObject};
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable};
+use serde::Serialize;
+
+use crate::schema::users;
+
+#[derive(Queryable, Serialize, SimpleObject)]
+#[diesel(table_name = users)]
+pub struct UserModel {
+    id: i32,
+    image_url: Option<String>,
+    name: String,
+    email: Option<String>,
+    #[graphql(skip)]
+    #[serde(skip)]
+    password: String,
+    #[graphql(skip)]
+    #[serde(skip)]
+    verified: Option<NaiveDateTime>,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable, InputObject)]
+#[diesel(table_name = users)]
+pub struct UserInput {
+    image_url: Option<String>,
+    name: String,
+    password: String,
+    verified: Option<NaiveDateTime>,
+}
